fc9e117f215fb2a9c122693ca833aa91fad80035
sunnyhitha <41725679+sunnyhitha@users.noreply.github.com>
2018-08-24 15:33:06 +0530

    Create 891.java

---
+++ 891.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String s;
+        Scanner ss=new Scanner(System.in);
+        s=ss.next();
+        char v[]=s.toCharArray();
+        Arrays.sort(v);
+        for(int i=0;i<v.length;i++){
+            System.out.print(v[i]);
+        }// your code goes here
+	}
+}
